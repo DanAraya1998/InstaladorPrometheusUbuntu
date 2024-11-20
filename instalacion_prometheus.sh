214480f73893cb36aa8f2f05b473c8e75d751c0d
DanAraya1998 <151966820+DanAraya1998@users.noreply.github.com>
2024-11-19 23:34:44 -0600

    Update instalacion_prometheus.sh

--- instalacion_prometheus.sh
+++ instalacion_prometheus.sh
@@ -4,6 +4,7 @@ echo "¡Comencemos a trabajar!"
 echo "//////////////////"
 sleep 3s
 echo "Inicialmente, se instalarán prometheus y algunas dependencias para los pasos siguientes..."
+sudo apt update
 sudo apt install -y wget tar prometheus prometheus-node-exporter apt-transport-https software-properties-common
 sleep 1s
 echo "Ahora, se recargan los daemons de systemctl y se arranca el servicio de Prometheus..."
@@ -12,7 +13,7 @@ sudo systemctl enable prometheus.service
 sudo systemctl start prometheus.service
 sleep 2s
 echo "Ahora, corresponde añadir el repo de Grafana a la sourcelist y realizar la instalación"
-wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key-add -
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 sudo apt update
 sudo apt install -y grafana
